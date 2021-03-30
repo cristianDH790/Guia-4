6c241df43114584f5607feabdda658dd037c7c08
User <User@DESKTOP-N9HSC28>
2021-03-30 13:53:58 -0500

    guia 4

--- src/Guia04/caso7.java
+++ src/Guia04/caso7.java
@@ -1,5 +1,6 @@
 package Guia04;
 
+import java.text.DecimalFormat;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Iterator;
@@ -13,9 +14,14 @@ public class caso7 {
 		//▪ El número menor. 
 		//▪ La sumatoria de todos los números. 
 		//▪ El promedio de todos los números.
-		//Creamos la instancia sc para crear el objeto Scanner
+		// Creamos la instancia sc para crear el objeto Scanner
 		Scanner sc = new Scanner(System.in);
+// Creamos la instancia df para crear el objeto DecimalFormat
+DecimalFormat df = new DecimalFormat("#0.00");	
+		
 		//Declaramos la matriz 
+		int mayor = 0, menor =0, suma = 0;
+		float pro = 0;
 		int [][]  numero = new int[3][4];
 		System.out.println("Lectura de números");
 		System.out.println("----------------------");
@@ -27,13 +33,27 @@ public class caso7 {
 			for ( int c = 0; c < numero [0].length; c++) {
 				System.out.print("Número " + (c + 1)+ ": ");
 			     numero [f] [c] = sc.nextInt();
+			     
+			     if ( c == 0 && f == 0) {
+			    	 mayor = numero [f] [c] ; 
+			        menor =numero [f] [c];
+			     }
+			     
+			     if ( mayor < numero [f] [c] ) mayor = numero [f] [c];
+			     if ( menor > numero [f] [c] ) menor = numero [f] [c];
+			     
+			     suma = suma +numero [f] [c];
+
 			}
 			System.out.println();
 		}
 		
+		pro = suma/ 12f;
 		
-		
-
+		System.out.println("Número mayor :"+ mayor);
+		System.out.println("Número menor :"+ menor);
+		System.out.println("Sumatoria     : "+ suma);
+		System.out.println("Promedio     : "+ df.format(pro));
 }
 
 }
